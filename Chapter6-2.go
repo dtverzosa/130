1bd66a99923ff6a368908af0c12d246057edb5ed
dtverzosa <dtverzosa@users.noreply.github.com>
2015-03-01 19:06:53 -0800

    Prints out the smallest number from a given array

---
+++ Chapter6-2.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+//Dustin Verzosa
+//This program prints out the smallest number in a given array
+func main() {
+  x := []int{ 48,96,86,68, 57,82,63,70, 37,34,83,27, 19,97, 9,17, }
+  y := x[0]
+  i:= 1
+  for i <= 15 {
+    if y > x[i] {
+    y = x[i]
+  }
+  i = i + 1
+  }
+  fmt.Println("Smallest number is", y)
+}
